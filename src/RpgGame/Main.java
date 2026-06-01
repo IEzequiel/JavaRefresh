c85867eb27ccbef5c4b78efe3d3454ebbe13741a
Ezequiel Ilivitzky <iaezequiel@gmail.com>
2026-06-01 16:51:24 -0300

    Primer commit: estructura base para el juego RPG de repaso de Java

---
+++ src/RpgGame/Main.java
@@ -0,0 +1,14 @@
+package RpgGame;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Character player1 = new Character("Arthas");
+        Character player2 = new Character("Geralt");
+        
+        player1.getStats();
+        player2.getStats();
+
+    }
+}
